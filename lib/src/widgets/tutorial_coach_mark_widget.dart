767695ac1a32460bf6b467349f6dc07991c3e22f
mohamed.haloka <mohamed.haloka@nahdetmisr.com>
2025-02-25 14:42:20 +0200

    remove ink well tap.

--- lib/src/widgets/tutorial_coach_mark_widget.dart
+++ lib/src/widgets/tutorial_coach_mark_widget.dart
@@ -274,16 +274,14 @@ class TutorialCoachMarkWidgetState extends State<TutorialCoachMarkWidget>
         child: AnimatedOpacity(
           opacity: showContent ? 1 : 0,
           duration: Durations.medium2,
-          child: IgnorePointer(
-            child: widget.skipWidget ??
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    widget.textSkip,
-                    style: widget.textStyleSkip,
-                  ),
+          child: widget.skipWidget ??
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  widget.textSkip,
+                  style: widget.textStyleSkip,
                 ),
-          ),
+              ),
         ),
       ),
     );
